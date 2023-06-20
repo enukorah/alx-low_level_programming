826c05da4e331bdde1cabbbc03e7368f679e0443
enukorah <enukorah@gmail.com>
2023-06-20 04:28:08 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char t;
+
+	int d;
+
+	t = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(t <= 'f') {
+			putchar(t);
+			t++;
+		}
+	putchar('\n');
+	return (0);
+}
+
